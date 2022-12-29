6bd30b783d69f0bbdfdf9afa4a6c91b147ef230d
Sergey Ryazanov <90528778+MajotraderLucky@users.noreply.github.com>
2022-12-29 21:21:27 +0300

    Add files via upload

--- algorithm.py
+++ algorithm.py
@@ -4,7 +4,8 @@ import time
 
 client = Client(keys.api_key, keys.api_secret)
 
-balance27_12_22 = -0.1722
+balance29_12_22 = 13.15539128
+strBalance29_12_22 = "29-12  eq = " + str(balance29_12_22)
 
 client = Client(keys.api_key, keys.api_secret)
 depth = client.get_order_book(symbol='ETHUSDT')
@@ -18,8 +19,8 @@ futAsks = futuresDepth['asks']
 balance = client.futures_account_balance()[6]['balance']
 printCurrentBalance = "Current balance = " + balance
 startBalanceFloat = float(balance)
-result = (startBalanceFloat - balance27_12_22)
-percentResult = ((result / balance27_12_22) * 100) * -1
+result = (startBalanceFloat - balance29_12_22)
+percentResult = ((result / balance29_12_22) * 100)
 printOverallResult = "The percentage overall result = \n     " + \
     str(percentResult) + "%"
 
--- main.py
+++ main.py
@@ -6,31 +6,61 @@ import time
 import os
 
 client = Client(keys.api_key, keys.api_secret)
-
-print("****This is spot spread****")
-print("---------------------------")
-print(algorithm.depthAsks[0][0], "-", algorithm.depthAsks[0][1])
-print("---------------------------")
-print(algorithm.depthBids[0][0], "-", algorithm.depthBids[0][1])
-print("---------------------------")
-print("---------------------------")
-
-print("***This is futures spread**")
-print("---------------------------")
-print("    ", algorithm.futAsks[0][0], "-", algorithm.futAsks[0][1])
-print("---------------------------")
-print("    ", algorithm.futBids[0][0], "-", algorithm.futBids[0][1])
-print("---------------------------")
-print("---------------------------")
-print(algorithm.printCurrentBalance)
-print(algorithm.printOverallResult)
-
+symbolEth = 'ETHUSDT'
+startPrice = client.futures_order_book(
+    symbol=symbolEth)['asks'][0][0]
+priceArr = []
+maxArr = []
+list_uniq_max_arr = []
 
 while True:
     for i in range(10, 0, -1):
-        print(f"Waiting: {i:2} cicles.")
-        print(f"", client.futures_order_book(symbol='ETHUSDT')['bids'][0][0])
-        a = client.futures_order_book(symbol='ETHUSDT')['bids'][0][0]
-        print(f"", a)
-        time.sleep(15)
+        print(f"------------------------")
+        print(f" --Waiting:{i:2} cicles--")
+        futBidPrise = "BID: " + client.futures_order_book(
+            symbol='ETHUSDT')['bids'][0][0] + " - " + client.futures_order_book(
+            symbol='ETHUSDT')['bids'][0][1]
+        futAskPrise = "ASK: " + client.futures_order_book(
+            symbol='ETHUSDT')['asks'][0][0] + " - " + client.futures_order_book(
+            symbol='ETHUSDT')['asks'][0][1]
+        spotAskPrice = "ASK: " + client.get_order_book(
+            symbol=symbolEth)['asks'][0][0]
+        volumeAskPrice = "Volume ask: " + client.get_order_book(
+            symbol=symbolEth)['asks'][0][1]
+        spotBidPrice = "BID: " + client.get_order_book(
+            symbol=symbolEth)['bids'][0][0]
+        volumeBidPrice = "Volume bid: " + client.get_order_book(
+            symbol=symbolEth)['bids'][0][1]
+        futBalanceNow = client.futures_account_balance()[6]['balance']
+        profit = float(futBalanceNow) - algorithm.balance29_12_22
+        profitPercent = (profit / algorithm.balance29_12_22) * 100
+        floatFutAskPrice = float(client.futures_order_book(
+            symbol=symbolEth)['asks'][0][0])
+        priceArr.append(floatFutAskPrice)
+        maxPrice = max(priceArr)
+        minPrice = min(priceArr)
+
+        print(f"------------------------")
+        print(f"----------spot----------")
+        print(f"------------------------")
+        print(f"", spotAskPrice)
+        print(f"", volumeAskPrice)
+        print(f"", spotBidPrice)
+        print(f"", volumeBidPrice)
+        print(f"------------------------")
+        print(f"---------futures--------")
+        print(f"------------------------")
+        print(f"", futAskPrise)
+        print(f"", futBidPrise)
+        print(f"------------------------")
+        print(f"", algorithm.strBalance29_12_22)
+        print(f"Equity now =", futBalanceNow)
+        print(f"My profit  =", str(profit) + "$")
+        print(f"Profit %   =", str(profitPercent) + "%")
+        print(f"------------------------")
+        print(f"Start price -", startPrice)
+        print(f"", priceArr)
+        print(f"high =", maxPrice)
+        print(f"low  =", minPrice)
+        time.sleep(20)
         os.system("clear")
